256384446a0dfe5664b9ea16220e1706d06400c5
Brendon Koh Keng Siong <brendon.koh.2014@sis.smu.edu.sg>
2016-06-23 21:03:05 +0800

    Admin Module+ Login Errors
    - Modified Admin Dashboard
    - Add login errors

--- app/app/controllers/sessions_controller.rb
+++ app/app/controllers/sessions_controller.rb
@@ -7,13 +7,19 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:email])    
     if user and user.authenticate(params[:password])      
-        session[:user_id] = user.id
-        #if user.is_admin
+        if user.is_banned          
+          flash[:alert]="Your account has been banned."
+          redirect_to action:"new"#, alert: "Your account has been banned."          
+        else
+        session[:user_id] = user.id        
+        if user.is_admin
           redirect_to :controller => 'admins', :action => 'index' and return
-        #else
-        #  redirect_to :controller => 'dashboards', :action => 'index' and return
-        #end        
+        else
+          redirect_to :controller => 'dashboards', :action => 'index' and return
+        end        
+      end
     else                
+        flash[:alert]="Invalid username/password."
         redirect_to action: "new"
     end
   end
