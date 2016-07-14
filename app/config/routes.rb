40913612318bcf5e24b81e27a71768d3ad375f3b
Brendon Koh Keng Siong <brendon.koh.2014@sis.smu.edu.sg>
2016-07-14 23:25:25 +0800

    Added project_members

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
   root 'pages#home' # root page
+
+  # project members routes  
+  get 'project_members/index' => 'project_members#index'
+  get 'project_members/new' => 'project_members#new'
+  get 'project_members/edit' => 'project_members#edit'  
+  post 'project_members' => 'project_members#create'  
+  #get 'project_members/edit' => => 'project_members#index'
+  post 'project_members/destroy/(:id)' => 'project_members#destroy'   
+
   resources :sessions, only: [:new, :create, :destroy] # only allow new,create,destroy action for sessions
   resources :users   
+  resources :project_members
   get 'signup' => 'users#new' # display create user page
-  post'users' => 'users#create' # process creation of user
+  post'users' => 'users#create' # process creation of user  
   get 'pages/comrules' => 'pages#comrules' # display com rules
   get 'pages/term' => 'pages#term' # display terms
   get 'login' => 'sessions#new' # show login form
@@ -13,5 +23,7 @@ Rails.application.routes.draw do
   get 'admins/index' => 'admins#index'
   get 'users/changepass/:id' => 'users#changepass'
   get 'admins/index' => "admins#index"
+   
+
   match '/:controller/:action/(:id)', via: [:get, :post] # last route
 end
