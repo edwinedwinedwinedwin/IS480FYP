d546e80d1e451e91cadc4ed6fcdecddcb82a8664
luqmanluqman <luqmant.2014@sis.smu.edu.sg>
2016-07-21 20:18:55 +0800

    update to project updates
    Rake DB:Migrate error solved by sky

--- app/db/migrate/20160719065630_update_columns_to_project_update.rb
+++
@@ -1,6 +0,0 @@
-class UpdateColumnstoProjectUpdate < ActiveRecord::Migration
-  def change
-    change_column_null :project_updates, :img_url, true
-    change_column_null :project_updates, :title, false
-  end
-end
