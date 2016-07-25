f416c49a9ae9dd78854571b6477a47999d74e6c6
luqmanluqman <luqmant.2014@sis.smu.edu.sg>
2016-07-25 22:44:32 +0800

    edited UI to add navigation bar
    added navigation bar to the top

--- app/app/models/project_reward.rb
+++ app/app/models/project_reward.rb
@@ -1,3 +1,6 @@
 class ProjectReward < ActiveRecord::Base
   belongs_to :project
+
+  #validates :name, :min_amount, :estimated_delivery, :no_of_rewards, :presence => { :message => " cannot be blank" },:on => :create
+  #validates :min_amount, :no_of_rewards, :numericality => true, :allow_nil => true
 end
