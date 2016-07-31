f4776eb30667f5346eef4780933cc20bb9f6a73c
Chew Zhi Xuan <zxchew.2014@sis.smu.edu.sg>
2016-07-31 14:58:32 +0800

    Update User
    User/Admin
    - remove address column
    - Seed File:  Create Default Admin Account
    - Add All Url Text Field to Edit User
    
    Project_Likes
    - Drop Table

--- app/db/seeds.rb
+++ app/db/seeds.rb
@@ -6,16 +6,20 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+
+User.create!(email: 'sean@ybco.com', first_name: 'Sean', last_name: 'Tan', is_admin: true, is_banned: false, password: '12345678', password_confirmation: '12345678')
+User.create!(email: 'uttam@ybco.com', first_name: 'Uttam', is_admin: true, is_banned: false, password: '12345678', password_confirmation: '12345678')
+
 #List of Project Category
 categoryNames = ["Technology", "Education", "Arts", "Entertainment", "Water & Sanitation", "Community", "Health & Fitness", "Finance"]
 categoryNames.each do |c|
-     ProjectCategory.create!( {:category => c})
+  ProjectCategory.create!( {:category => c})
 end
 
 #List of Project Status
 statusList = ["In progress", "Pending", "Approved", "Rejected", "Completed"]
 statusList.each do |s|
-	ProjectStatus.create!( {:status => s})
+  ProjectStatus.create!( {:status => s})
 end
 
 #List of Project Types
