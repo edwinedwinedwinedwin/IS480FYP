93b5129939cb89702df0832e872f26203ed7a016
Chew Zhi Xuan <zxchew.2014@sis.smu.edu.sg>
2016-08-16 05:10:48 +0800

    Update GitIgnore

--- app/db/schema.rb
+++ app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160815170448) do
+ActiveRecord::Schema.define(version: 20160815193419) do
 
   create_table "project_categories", force: :cascade do |t|
     t.string   "category",   limit: 255
@@ -31,16 +31,6 @@ ActiveRecord::Schema.define(version: 20160815170448) do
 
   add_index "project_inspirations", ["project_id"], name: "fk_rails_d433d28e4f", using: :btree
 
-  create_table "project_likes", force: :cascade do |t|
-    t.integer  "user_id",    limit: 4
-    t.integer  "project_id", limit: 4
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
-  end
-
-  add_index "project_likes", ["project_id"], name: "fk_rails_513d01bb04", using: :btree
-  add_index "project_likes", ["user_id"], name: "fk_rails_8db23f111d", using: :btree
-
   create_table "project_members", force: :cascade do |t|
     t.string   "role",              limit: 255
     t.integer  "project_id",        limit: 4
@@ -50,6 +40,7 @@ ActiveRecord::Schema.define(version: 20160815170448) do
     t.integer  "project_status_id", limit: 4
     t.integer  "user_id",           limit: 4
     t.string   "second_role",       limit: 255
+    t.string   "email",             limit: 255
   end
 
   add_index "project_members", ["project_id"], name: "fk_rails_f3b43b5269", using: :btree
@@ -193,7 +184,6 @@ ActiveRecord::Schema.define(version: 20160815170448) do
     t.string   "email",           limit: 255
     t.string   "first_name",      limit: 255
     t.string   "last_name",       limit: 255
-    t.string   "address",         limit: 255
     t.boolean  "is_admin"
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
@@ -207,8 +197,6 @@ ActiveRecord::Schema.define(version: 20160815170448) do
   end
 
   add_foreign_key "project_inspirations", "projects"
-  add_foreign_key "project_likes", "projects"
-  add_foreign_key "project_likes", "users"
   add_foreign_key "project_members", "project_statuses"
   add_foreign_key "project_members", "projects"
   add_foreign_key "project_members", "users"
