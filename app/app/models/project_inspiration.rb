6cb6152a3277eb9f3526c59a9cfd73d7e8c91137
Chew Zhi Xuan <zxchew.2014@sis.smu.edu.sg>
2016-08-16 05:48:55 +0800

    Added Project Update into Project

--- app/app/models/project_inspiration.rb
+++ app/app/models/project_inspiration.rb
@@ -2,6 +2,7 @@ class ProjectInspiration < ActiveRecord::Base
   belongs_to :project
 
   mount_uploader :img_url, ProjectInspirationImgUploader
-  validates :img_url, file_size: { less_than: 2.megabytes }  
+  validates :img_url, file_size: { less_than: 2.megabytes }
+  validates :title, :description, :presence => true
 end
 
