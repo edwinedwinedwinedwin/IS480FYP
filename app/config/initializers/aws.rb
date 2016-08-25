4b8c36d549eb87af2dfe220a48fa8964440c12b5
Brendon Koh Keng Siong <brendon.koh.2014@sis.smu.edu.sg>
2016-08-25 16:14:20 +0800

    Uploaded Image Function
    Uncommit credentials

--- app/config/initializers/aws.rb
+++ app/config/initializers/aws.rb
@@ -1,6 +1,6 @@
 Aws.config.update({
   region: 'us-east-1',
-  credentials: Aws::Credentials.new(ENV['AKIAIQNXC56XKQD6X4KQ'], ENV['2RHnbGinLWBjxJVutu9WAHsYSlu/t0KSr6mxJ13E']),
+  #credentials: Aws::Credentials.new(ENV['AKIAIQNXC56XKQD6X4KQ'], ENV['2RHnbGinLWBjxJVutu9WAHsYSlu/t0KSr6mxJ13E']),
 })
 
 S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['S3_BUCKET'])
--- app/config/initializers/s3.rb
+++ app/config/initializers/s3.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
       :provider               => 'AWS',
-      :aws_access_key_id      => "AKIAIQNXC56XKQD6X4KQ",
-      :aws_secret_access_key  => "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",
+      #:aws_access_key_id      => "AKIAIQNXC56XKQD6X4KQ",
+      #:aws_secret_access_key  => "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",
       :region                 => 'ap-southeast-1' # Change this for different AWS region. Default is 'us-east-1'
   }
   config.fog_directory  = 'ybcoupload'  
