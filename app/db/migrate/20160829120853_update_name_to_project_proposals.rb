a5cdfa15d92fe7eab97ccd941f3f6fc3e521da69
Chanel <chanelc.2014@sis.smu.edu.sg>
2016-09-06 12:36:51 +0800

    Revert "Solved Rake db migrate issue"
    This reverts commit da01b580a6d815da89e3451cc06636c780146891.

--- app/db/migrate/20160829120853_update_name_to_project_proposals.rb
+++ app/db/migrate/20160829120853_update_name_to_project_proposals.rb
@@ -1,6 +1,5 @@
 class UpdateNameToProjectProposals < ActiveRecord::Migration
   def change
-  	remove_foreign_key :project_proposals, :project_types
   	remove_column :project_proposals, :project_type_id
   	remove_column :project_proposals,:name
   	add_column :project_proposals , :first_name, :string
