3dab75e66f08f427d270574a13a24687d5b1f037
edwin <edwin.peter.2014@sis.smu.edu.sg>
2016-09-27 07:17:59 +0800

    Commit country select ui change
    FOR EDWIN BY EDWIN WEW

--- app/app/assets/javascripts/main.js
+++ app/app/assets/javascripts/main.js
@@ -260,6 +260,11 @@ $(function() {
      	}
     });
 
+    $('#resizing_select').change(function(){
+      $("#width_tmp_option").html($('#resizing_select option:selected').text()); 
+      $(this).width($("#width_tmp_select").width());  
+    });
+
 });
 
 /* for bxslider
