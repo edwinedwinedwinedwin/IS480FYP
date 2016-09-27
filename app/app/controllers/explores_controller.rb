6b32343a9260fec72faf5d1b69cbadb8c4940eac
edwin <edwin.peter.2014@sis.smu.edu.sg>
2016-09-27 22:15:19 +0800

    COMMIT MINE IS THE LATEST ONE LA
    for edwin

--- app/app/controllers/explores_controller.rb
+++ app/app/controllers/explores_controller.rb
@@ -1,17 +1,20 @@
 class ExploresController < ApplicationController
   def index
     @projects = Project.all
-    @session = session[:user_id]
+
+    current_User = session[:user_id]
+
   end
 
   def show
     @project = Project.find_by(:id => params[:id])
-    @session = session[:user_id]
-   	checkCreator = ProjectMember.find_by(:user_id => @session, :project_id => @project.id)
+   
+   @current_User = session[:user_id]
+
+    checkCreator = ProjectMember.find_by(:user_id => @current_User, :project_id => @project.id)
     if !checkCreator.nil?
       redirect_to showProject_path(:id => @project.id)
     end
-
     @project_proposal = ProjectProposal.find(@project.project_proposal_id)
     @user = User.find(@project.user_id)
     @project_proposal_imgs = ProjectProposalImg.where(:project_proposal_id => @project_proposal.id)
@@ -31,10 +34,14 @@ class ExploresController < ApplicationController
     @project_updates = ProjectUpdate.order('id DESC').where(:project_id => params[:id])
     @project_rewards = ProjectReward.where(:project_id => params[:id])
     @project_milestones = ProjectMilestone.where(:project_id => params[:id])
+    #@project_milestones_start = ProjectMilestone.find_by_project_id(params[:id]).first
+    #@project_milestones_end = ProjectMilestone.find_by_project_id(params[:id]).last
 
     if !session[:user_id].nil?
+      @new_reward = ProjectReward.new
+      @new_update = ProjectUpdate.new
 
-     @projects=ProjectProposal.select("*").joins(:project).where(:projects => {:user_id=>@session})
+      @projects=ProjectProposal.select("*").joins(:project).where(:projects => {:user_id=>@session})
       @project_coverImgs = ProjectProposalImg.select('
                   project_proposal_imgs.project_proposal_id as pp_id,
                   project_proposal_imgs.id as ppi_id,
