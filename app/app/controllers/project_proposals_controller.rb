071045eb2adf0e2e2b8ae3d2aa6f843c8f3175dd
Chew Zhi Xuan <zxchew.2014@sis.smu.edu.sg>
2016-09-27 20:34:00 +0800

    Retrieve Details of User in New Proposal

--- app/app/controllers/project_proposals_controller.rb
+++ app/app/controllers/project_proposals_controller.rb
@@ -56,7 +56,21 @@ class ProjectProposalsController < ApplicationController
   end
 
   def successProposal
+    @session =session[:user_id]
     @ProjectProposal = ProjectProposal.find(params[:id])
+
+    if !session[:user_id].nil?
+      @session=session[:user_id]
+
+      @projects=ProjectProposal.select("*").joins(:project).where(:projects => {:user_id=>@session})
+      @project_coverImgs = ProjectProposalImg.select('
+                  project_proposal_imgs.project_proposal_id as pp_id,
+                  project_proposal_imgs.id as ppi_id,
+                  project_proposals.title as title,
+                  projects.id as p_id
+                  ').joins(project_proposal: :project).where(:projects => {:user_id => @session})
+      @user=User.find(@session) # only able to edit current logged in user
+    end
   end
 
   def accept
