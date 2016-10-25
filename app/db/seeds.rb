4a6fe2591c521183b867a6a6cadad6351acec82b
edwin <edwin.peter.2014@sis.smu.edu.sg>
2016-10-25 22:06:40 +0800

    commit seeds
    for db

--- app/db/seeds.rb
+++ app/db/seeds.rb
@@ -7,13 +7,13 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 #List of Admin
-User.create!(email: 'sean@ybco.com', first_name: 'Sean', last_name: 'Low', is_admin: true, is_banned: false, password: '12345678', password_confirmation: '12345678')
-User.create!(email: 'uttam@ybco.com', first_name: 'Uttam', last_name: 'Chopra',is_admin: true, is_banned: false, password: '12345678', password_confirmation: '12345678')
-User.create!(email: 'sky@ybco.com', first_name: 'Sky', last_name: 'Chew', is_admin: true, is_banned: false, password: '12345678', password_confirmation: '12345678')
-User.create!(email: 'luqman@ybco.com', first_name: 'Luqman', is_admin: true, is_banned: false, password: '12345678', password_confirmation: '12345678')
-User.create!(email: 'edwin@ybco.com', first_name: 'Edwin', last_name: 'Peter', is_admin: true, is_banned: false, password: '12345678', password_confirmation: '12345678')
-User.create!(email: 'brendon@ybco.com', first_name: 'Brendon', last_name: 'Koh', is_admin: true, is_banned: false, password: '12345678', password_confirmation: '12345678')
-User.create!(email: 'chanel@ybco.com', first_name: 'Chanel', last_name: 'Chiang', is_admin: true, is_banned: false, password: '12345678', password_confirmation: '12345678')
+User.create!(email: 'sean@ybco.com', name: 'Sean Low', is_admin: true, is_banned: false, password: '12345678', password_confirmation: '12345678', fb_url: 'www.facebook.com')
+User.create!(email: 'uttam@ybco.com', name: 'Uttam Chopra',is_admin: true, is_banned: false, password: '12345678', password_confirmation: '12345678', fb_url: 'www.facebook.com')
+User.create!(email: 'sky@ybco.com', name: 'Sky Chew', is_admin: true, is_banned: false, password: '12345678', password_confirmation: '12345678', fb_url: 'www.facebook.com')
+User.create!(email: 'luqman@ybco.com', name: 'Luqman', is_admin: true, is_banned: false, password: '12345678', password_confirmation: '12345678', fb_url: 'www.facebook.com')
+User.create!(email: 'edwin@ybco.com', name: 'Edwin Peter', is_admin: true, is_banned: false, password: '12345678', password_confirmation: '12345678', fb_url: 'www.facebook.com')
+User.create!(email: 'brendon@ybco.com', name: 'Brendon Koh', is_admin: true, is_banned: false, password: '12345678', password_confirmation: '12345678', fb_url: 'www.facebook.com')
+User.create!(email: 'chanel@ybco.com', name: 'Chanel Chiang', is_admin: true, is_banned: false, password: '12345678', password_confirmation: '12345678', fb_url: 'www.facebook.com')
 
 #List of Project Category
 categoryNames = ["Technology", "Education", "Arts", "Entertainment", "Water & Sanitation", "Community", "Health & Fitness", "Finance"]
