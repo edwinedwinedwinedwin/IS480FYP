42797f96974529ebaf8acfd1292e2acbb3fca96f
edwin <edwin.peter.2014@sis.smu.edu.sg>
2016-11-12 21:08:18 +0800

    commit show password

--- app/app/assets/javascripts/main.js
+++ app/app/assets/javascripts/main.js
@@ -236,25 +236,26 @@ $(function() {
 	});
 
 	// Banner notice
+	/*
 	$('.banner-notice .js-close').on('click', function() {
 		$(this).parents('.banner-notice').hide();
 		$('.page-header').css('top', '2rem');
 		$projectNavigation.css('top', '0').sticky({
 			topSpacing: 0
 		});
-	});
+	});*/
 
 	
 
     $(".dropdown-toggle").dropdown();
     $('#admin-project-proposal-table').DataTable();
 
-
+    
     $(window).scroll(function(){
         if ($(this).scrollTop()>0){
-        	$('#banner').fadeOut();
-        	$('.banner-notice').fadeOut();
-            	$('.page-header').css('top', '1rem');
+        	//$('#banner').fadeOut();
+        	//$('.banner-notice').fadeOut();
+            	//$('.page-header').css('top', '1rem');
     			$projectNavigation.css('top', '0').sticky({
     				topSpacing: 0
 				});
@@ -315,9 +316,67 @@ $(function() {
       sort();
     },
 
-  });
+  	});
 
-  	
+  	var acc = document.getElementsByClassName("accordion");
+  	var i;
+
+  	for (i = 0; i < acc.length; i++) {
+  	    acc[i].onclick = function(){
+  	        this.classList.toggle("active");
+  	        this.nextElementSibling.classList.toggle("show");
+  	  }
+  	}
+
+  	jQuery(document).ready(function($){
+  		var contentSections = $('.cd-section'),
+  			navigationItems = $('#cd-vertical-nav a');
+
+  		updateNavigation();
+  		$(window).on('scroll', function(){
+  			updateNavigation();
+  		});
+
+  		//smooth scroll to the section
+  		navigationItems.on('click', function(event){
+  	        event.preventDefault();
+  	        smoothScroll($(this.hash));
+  	    });
+  	    //smooth scroll to second section
+  	    $('.cd-scroll-down').on('click', function(event){
+  	        event.preventDefault();
+  	        smoothScroll($(this.hash));
+  	    });
+
+  	    //open-close navigation on touch devices
+  	    $('.touch .cd-nav-trigger').on('click', function(){
+  	    	$('.touch #cd-vertical-nav').toggleClass('open');
+  	  
+  	    });
+  	    //close navigation on touch devices when selectin an elemnt from the list
+  	    $('.touch #cd-vertical-nav a').on('click', function(){
+  	    	$('.touch #cd-vertical-nav').removeClass('open');
+  	    });
+
+  		function updateNavigation() {
+  			contentSections.each(function(){
+  				$this = $(this);
+  				var activeSection = $('#cd-vertical-nav a[href="#'+$this.attr('id')+'"]').data('number') - 1;
+  				if ( ( $this.offset().top - $(window).height()/2 < $(window).scrollTop() ) && ( $this.offset().top + $this.height() - $(window).height()/2 > $(window).scrollTop() ) ) {
+  					navigationItems.eq(activeSection).addClass('is-selected');
+  				}else {
+  					navigationItems.eq(activeSection).removeClass('is-selected');
+  				}
+  			});
+  		}
+
+  		function smoothScroll(target) {
+  	        $('body,html').animate(
+  	        	{'scrollTop':target.offset().top},
+  	        	600
+  	        );
+  		}
+  	});
 
 
 });
