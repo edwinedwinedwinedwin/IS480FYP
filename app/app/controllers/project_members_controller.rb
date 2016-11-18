16c3c818f51685357dc028d823994090680e510d
Chew Zhi Xuan <zxchew.2014@sis.smu.edu.sg>
2016-11-19 01:43:09 +0800

    Project Member and HeaderUser (Update)

--- app/app/controllers/project_members_controller.rb
+++ app/app/controllers/project_members_controller.rb
@@ -57,6 +57,7 @@ class ProjectMembersController < ApplicationController
     #redirect_to :controller => 'project_members', :action => 'index'
     redirect_to projectMembersIndex_path and return
   end
+
   private
   def project_members_params
     params.require(:project_member).permit(:email,:user_id, :second_role,:role,:project_id,:project_status_id)
