05416265aa6fe1ffdb127d54bf05f30582ed4dfe
Chew Zhi Xuan <zxchew.2014@sis.smu.edu.sg>
2016-11-20 01:28:55 +0800

    db migrate it

--- app/db/migrate/20161117151229_add_columns_to_proposals.rb
+++ app/db/migrate/20161117151229_add_columns_to_proposals.rb
@@ -1,13 +1,13 @@
 class AddColumnsToProposals < ActiveRecord::Migration
   def change
-    #add_column :project_proposals, :company_url, :string
+    add_column :project_proposals, :company_url, :string
     add_column :project_proposals, :creator_title, :string
     add_column :project_proposals, :estimate_amt, :decimal, precision: 10, scale: 2
-    #change_column :project_reward_backers, :amount_funded, :decimal, precision: 10, scale: 2
-    #change_column :project_rewards, :min_amount, :decimal, precision: 10, scale: 2
+    change_column :project_reward_backers, :amount_funded, :decimal, precision: 10, scale: 2
+    change_column :project_rewards, :min_amount, :decimal, precision: 10, scale: 2
 
-    #change_column :project_milestones, :amount_raised, :decimal, precision: 10, scale: 2
-    #change_column :project_milestones, :target_amount, :decimal, precision: 10, scale: 2
+    change_column :project_milestones, :amount_raised, :decimal, precision: 10, scale: 2
+    change_column :project_milestones, :target_amount, :decimal, precision: 10, scale: 2
 
     #remove_column :project_proposals, :contact_number
     add_column :project_proposals, :estimated_start_date, :date
