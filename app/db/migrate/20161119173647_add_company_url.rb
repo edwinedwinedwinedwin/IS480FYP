0df7df2825c41640ce36d32d3d9ece845f874ab4
Chew Zhi Xuan <zxchew.2014@sis.smu.edu.sg>
2016-11-20 01:44:55 +0800

    db

--- app/db/migrate/20161119173647_add_company_url.rb
+++ app/db/migrate/20161119173647_add_company_url.rb
@@ -1,9 +1,9 @@
 class AddCompanyUrl < ActiveRecord::Migration
   def change
-   # change_column :project_reward_backers, :amount_funded, :decimal, precision: 10, scale: 2
-   # change_column :project_rewards, :min_amount, :decimal, precision: 10, scale: 2
-   # change_column :project_milestones, :amount_raised, :decimal, precision: 10, scale: 2
-   # change_column :project_milestones, :target_amount, :decimal, precision: 10, scale: 2
-   # add_column :project_proposals, :company_url, :string
+    change_column :project_reward_backers, :amount_funded, :decimal, precision: 10, scale: 2
+    change_column :project_rewards, :min_amount, :decimal, precision: 10, scale: 2
+    change_column :project_milestones, :amount_raised, :decimal, precision: 10, scale: 2
+    change_column :project_milestones, :target_amount, :decimal, precision: 10, scale: 2
+    add_column :project_proposals, :company_url, :string
   end
 end
