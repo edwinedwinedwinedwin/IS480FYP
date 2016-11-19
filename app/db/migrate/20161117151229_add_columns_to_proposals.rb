14c1e290aa3edb269aa5b00c7c0d57fd8c8f1965
Chew Zhi Xuan <zxchew.2014@sis.smu.edu.sg>
2016-11-20 01:29:39 +0800

    db again

--- app/db/migrate/20161117151229_add_columns_to_proposals.rb
+++ app/db/migrate/20161117151229_add_columns_to_proposals.rb
@@ -9,7 +9,7 @@ class AddColumnsToProposals < ActiveRecord::Migration
     change_column :project_milestones, :amount_raised, :decimal, precision: 10, scale: 2
     change_column :project_milestones, :target_amount, :decimal, precision: 10, scale: 2
 
-    #remove_column :project_proposals, :contact_number
+    remove_column :project_proposals, :contact_number
     add_column :project_proposals, :estimated_start_date, :date
     add_column :project_proposals, :estimated_end_date, :date
     rename_column :project_proposals, :estimate_amt ,:estimated_amt_raise
