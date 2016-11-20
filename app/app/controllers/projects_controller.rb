98113c2661a95ef5a5f0cfcd8d63f6a7984ae4ce
Chew Zhi Xuan <zxchew.2014@sis.smu.edu.sg>
2016-11-20 16:33:01 +0800

    Project Member

--- app/app/controllers/projects_controller.rb
+++ app/app/controllers/projects_controller.rb
@@ -28,7 +28,7 @@ before_filter :logged_in,:authorize_admin, only: [:accept ,:reject, :index]
     if !session[:user_id].nil?
 
       @session = session[:user_id]
-      @project_requests = ProjectMember.where(:user_id => @session, :project_status_id => 2)
+      @project_requests = ProjectMember.order('project_status_id ASC').where(:user_id => @session, :project_status_id => [2,3])
 
 
       @projects=ProjectProposal.select("*").joins(:project).where(:projects => {:user_id=>@session})
@@ -204,8 +204,6 @@ before_filter :logged_in,:authorize_admin, only: [:accept ,:reject, :index]
     if !session[:user_id].nil?
 
       @session = session[:user_id]
-      @project_requests = ProjectMember.where(:user_id => @session, :project_status_id => 2)
-
 
       @projects=ProjectProposal.select("*").joins(:project).where(:projects => {:user_id=>@session})
 
@@ -225,8 +223,6 @@ before_filter :logged_in,:authorize_admin, only: [:accept ,:reject, :index]
     if !session[:user_id].nil?
 
       @session = session[:user_id]
-      @project_requests = ProjectMember.where(:user_id => @session, :project_status_id => 2)
-
 
       @projects=ProjectProposal.select("*").joins(:project).where(:projects => {:user_id=>@session})
 
