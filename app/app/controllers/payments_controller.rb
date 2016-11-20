6b8d52b88f329b4920fab0b4522b574c1c39c870
Brendon Koh Keng Siong <brendon.koh.2014@sis.smu.edu.sg>
2016-11-20 14:57:48 +0800

    Completed Payment module function

--- app/app/controllers/payments_controller.rb
+++ app/app/controllers/payments_controller.rb
@@ -9,26 +9,51 @@ class PaymentsController < ApplicationController
 
   def new     
      @current_milestone = ProjectMilestone.order('start_date ASC').where(:project_id => params[:id]).where.not(:project_status_id => 5).first
-  	 @project_reward = ProjectReward.find(params[:pr_id])  	 
-  	 @user = User.find(session[:user_id])  	 
-  	 #@user_shipping_addresses = UserShippingAddress.where(:user_id=>@user.id)
-     @user_shipping_address = UserShippingAddress.find_by_user_id(@user.id)
-  	 @Payment = Payment.new
+     @project_reward = ProjectReward.find(params[:pr_id])    
+     @user = User.find(session[:user_id])           
+     @user_shipping_addresses = UserShippingAddress.where(:user_id=>session[:user_id])
+     @Payment = Payment.new
+      if !session[:user_id].nil?
+
+      @session = session[:user_id]
+      @project_requests = ProjectMember.where(:user_id => @session, :project_status_id => 2)
+
+
+      @projects=ProjectProposal.select("*").joins(:project).where(:projects => {:user_id=>@session})
+
+      @project_coverImgs = ProjectProposalImg.select('
+                  project_proposal_imgs.project_proposal_id as pp_id,
+                  project_proposal_imgs.id as ppi_id,
+                  project_proposals.title as title,
+                  projects.id as p_id
+                  ').joins(project_proposal: :project).where(:projects => {:user_id => @session})      
+    end
   end
 
-  def create        
-  	@pr=ProjectReward.find(params[:payment][:project_reward_id])        
+  def create           
+    @payment = Payment.new(payment_params) 
+    @user_shipping_address_id = params[:payment][:user_shipping_address_id]
+    if @user_shipping_address_id.nil
+      @user_shipping_address = UserShippingAddress.new
+      @user_shipping_address.address_line_1 = params[:payment][:address_line_1]
+      @user_shipping_address.address_line_2 = params[:payment][:address_line_2]
+      @user_shipping_address.country = params[:payment][:country]
+      @user_shipping_address.city = params[:payment][:city]
+      @user_shipping_address.state = params[:payment][:state]
+      @user_shipping_address.postal_code = params[:payment][:postal_code]
+      @user_shipping_address.user_id = session[:user_id]
+      @user_shipping_address.save        
+      @payment.user_shipping_address_id= @user_shipping_address.id
+    else
+      @payment.user_shipping_address_id= params[:payment][:user_shipping_address_id]
+    end
+    @pr=ProjectReward.find(params[:payment][:project_reward_id])        
     @projectID=Project.find(@pr.project_id).id
-  	@payment = Payment.new(payment_params)  
-    #if params[:payment][:amount]<@pr.min_amount
-    #  flash[:alert]="Amount cannot be less than ".to_s+@pr.min_amount.to_s
-    #  render 'new'      
-    #end
-    
+
     if @payment.save
-    	#ProjectReward.update(@pr.id,:no_of_rewards=>(@pr.no_of_rewards-1))
-    	redirect_to @payment.paypal_url(viewProject_path(@projectID)) and return
-    else    	      
+      #ProjectReward.update(@pr.id,:no_of_rewards=>(@pr.no_of_rewards-1))
+      redirect_to @payment.paypal_url(viewProject_path(@projectID)) and return
+    else            
       redirect_to makePayment_path(:pr_id=>@pr.id) and return
     end 
   end 
