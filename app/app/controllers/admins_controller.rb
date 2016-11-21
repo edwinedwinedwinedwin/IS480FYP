8e2ebf7db51c54594e93bc81e105fb9b5772ee86
Chew Zhi Xuan <zxchew.2014@sis.smu.edu.sg>
2016-11-21 20:01:25 +0800

    admin and milestone

--- app/app/controllers/admins_controller.rb
+++ app/app/controllers/admins_controller.rb
@@ -3,6 +3,10 @@ class AdminsController < ApplicationController
   def index
     @current_User = User.find(session[:user_id])
     @ProjectProposals = ProjectProposal.order('project_proposals.created_at DESC').all
+    @ProjectOnLive = Project.where(:project_status_id => 7).count()
+    @paymentRaised = Payment.sum(:amount)
+    @targetAmt = ProjectMilestone.sum(:target_amount)
+    @noOfUsers = User.count(:id)
   end
 
   def new
