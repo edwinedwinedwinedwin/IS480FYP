609d4fc0973ca312ba97a30c19f1271f95888e2a
Brendon Koh Keng Siong <brendon.koh.2014@sis.smu.edu.sg>
2016-11-30 17:34:49 +0800

    Solved payment error

--- app/app/models/payment.rb
+++ app/app/models/payment.rb
@@ -1,10 +1,9 @@
 class Payment < ActiveRecord::Base
 
-    #validates :user_shipping_address_id, presence: { message: "Please create a user shipping address." }
-
+    #validates :user_shipping_address_id, presence: { message: "Please create a user shipping address." }    
 	def paypal_url(return_path)    
     values = {
-        business: "ybcosg-facilitator@gmail.com",
+        business: "ybcosg@gmail.com",
         cmd: "_xclick",
         upload: 1,
         return: "#{Rails.application.secrets.app_host}#{return_path}",
@@ -13,6 +12,7 @@ class Payment < ActiveRecord::Base
         item_name: self.project_reward_name,
         item_number: self.project_reward_id,
         quantity: '1'
+        #notify_url: "#{Rails.application.secrets.app_host}/hook"
     }
     "#{Rails.application.secrets.paypal_host}/cgi-bin/webscr?" + values.to_query
   end
